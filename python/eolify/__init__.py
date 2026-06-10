c0f3c883fd36ef3ae16d93eed4e96c60b6374408
M. Swaanenburg <mark@swaanenburg.nl>
2026-06-10 20:12:33 +0200

    Use `inspect` feature to generate type hints from Rust
    This does require maturing to be invoked with
    `--generate-stubs`.

--- python/eolify/__init__.py
+++
@@ -1,5 +0,0 @@
-from .eolify import *
-
-__doc__ = eolify.__doc__
-if hasattr(eolify, "__all__"):
-    __all__ = eolify.__all__
--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,69 @@
-use std::{fs, io, path};
-
-use eolify::{IoExt, Normalize};
-use pyo3::{
-    Bound, PyResult, pyclass, pyfunction, pymodule,
-    types::{PyModule, PyModuleMethods},
-    wrap_pyfunction,
-};
-
-#[pyclass(eq, eq_int, from_py_object)]
-#[derive(Clone, Copy, PartialEq)]
-#[allow(clippy::upper_case_acronyms)]
-/// Line endings normalization mode.
-enum Mode {
-    LF,
-    CRLF,
-}
-
-#[pyfunction]
-#[pyo3(signature = (text, mode))]
-/// Normalize line endings in text
-///
-/// Args:
-///     text: Input text
-///     mode: Desired line ending style
-///
-/// Returns:
-///     Normalized text
-fn normalize_text(text: &str, mode: Mode) -> String {
-    match mode {
-        Mode::LF => eolify::LF::normalize_str(text),
-        Mode::CRLF => eolify::CRLF::normalize_str(text),
-    }
-}
-
-#[pyfunction]
-#[pyo3(signature = (input, output, mode, overwrite = false))]
-/// Normalize line endings from a file
-///
-/// Args:
-///     input: Path to source file
-///     output: Path destination file
-///     mode: Desired line ending style
-///     overwrite: Allow existing output files to be overwritten (default = False)
-fn normalize_file(
-    input: path::PathBuf,
-    output: path::PathBuf,
-    mode: Mode,
-    overwrite: bool,
-) -> PyResult<()> {
-    let input = fs::File::open(input)?;
-    let mut output = if overwrite {
-        fs::File::create(output)?
-    } else {
-        fs::File::create_new(output)?
-    };
-
-    let mut input: Box<dyn io::Read> = match mode {
-        Mode::LF => Box::new(eolify::LF::wrap_reader(input)),
-        Mode::CRLF => Box::new(eolify::CRLF::wrap_reader(input)),
-    };
-    io::copy(&mut input, &mut output)?;
-
-    Ok(())
-}
+use pyo3::pymodule;
 
 #[pymodule(name = "eolify")]
-fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<Mode>()?;
-    m.add_function(wrap_pyfunction!(normalize_text, m)?)?;
-    m.add_function(wrap_pyfunction!(normalize_file, m)?)?;
-    Ok(())
+mod eolify_py {
+    use std::{fs, io, path};
+
+    use eolify::{IoExt, Normalize};
+    use pyo3::{PyResult, Python, pyclass, pyfunction};
+
+    #[pyclass(eq, eq_int, from_py_object)]
+    #[derive(Clone, Copy, PartialEq)]
+    #[allow(clippy::upper_case_acronyms)]
+    /// Line endings normalization mode.
+    enum Mode {
+        LF,
+        CRLF,
+    }
+
+    #[pyfunction]
+    #[pyo3(signature = (text: "str", mode: "Mode") -> "str")]
+    /// Normalize line endings in text
+    ///
+    /// Args:
+    ///     text: Input text
+    ///     mode: Desired line ending style
+    ///
+    /// Returns:
+    ///     Normalized text
+    fn normalize_text(text: &str, mode: Mode) -> String {
+        match mode {
+            Mode::LF => eolify::LF::normalize_str(text),
+            Mode::CRLF => eolify::CRLF::normalize_str(text),
+        }
+    }
+
+    #[pyfunction]
+    #[pyo3(signature = (input: "str | PathLike[str]", output: "str | PathLike[str]", mode: "More", overwrite: "bool" = false))]
+    /// Normalize line endings from a file
+    ///
+    /// Args:
+    ///     input: Path to source file
+    ///     output: Path destination file
+    ///     mode: Desired line ending style
+    ///     overwrite: Allow existing output files to be overwritten (default = False)
+    fn normalize_file(
+        py: Python<'_>,
+        input: path::PathBuf,
+        output: path::PathBuf,
+        mode: Mode,
+        overwrite: bool,
+    ) -> PyResult<()> {
+        py.detach(|| {
+            let input = fs::File::open(input)?;
+            let mut output = if overwrite {
+                fs::File::create(output)?
+            } else {
+                fs::File::create_new(output)?
+            };
+
+            let mut input: Box<dyn io::Read> = match mode {
+                Mode::LF => Box::new(eolify::LF::wrap_reader(input)),
+                Mode::CRLF => Box::new(eolify::CRLF::wrap_reader(input)),
+            };
+
+            io::copy(&mut input, &mut output)?;
+            Ok(())
+        })
+    }
 }
